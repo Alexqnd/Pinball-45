fdd02f76fefd75ff68c55ee6b41ea0cc81ac109f
Alexander Quindt <alexander.quindt@students.tbs1.de>
2022-02-25 19:49:51 +0100

    Ball gets redeployed after certain time for testing

--- pinball_hz.py
+++ pinball_hz.py
@@ -40,6 +40,25 @@ class Ball(pygame.sprite.Sprite):
         new_direction_y = self.direction[1]
         self.direction = (new_direction_x, new_direction_y)
 
+class Timer(object):
+    def __init__(self, duration, with_start = True):
+        self.duration = duration
+        if with_start:
+            self.next = pygame.time.get_ticks()
+        else:
+            self.next = pygame.time.get_ticks() + self.duration
+
+    def is_next_stop_reached(self):
+        if pygame.time.get_ticks() > self.next:
+            self.next = pygame.time.get_ticks() + self.duration
+            return True
+        return False
+
+    def change_duration(self, delta=10):
+        self.duration += delta
+        if self.duration < 0:
+            self.duration = 0
+
 class AutoDeploy360(pygame.sprite.Sprite):
     def __init__(self, ball, pos_x, pos_y, angle, force) -> None:
         super().__init__()
@@ -55,6 +74,15 @@ class AutoDeploy360(pygame.sprite.Sprite):
         self.ball.sprite.direction = (direction_x, direction_y) 
         self.ball.sprite.rect.center = (self.pos_x, self.pos_y)
 
+class ReDeploy360(AutoDeploy360):
+    def __init__(self, ball, pos_x, pos_y, angle, force, time) -> None:
+        super().__init__(ball, pos_x, pos_y, angle, force)
+        self.timer = Timer(time)
+
+    def redeploy(self) -> None:
+        if self.timer.is_next_stop_reached():
+            self.deploy()
+
     
 class Game(object):
     def __init__(self):
@@ -65,12 +93,11 @@ class Game(object):
         pygame.display.set_caption(Settings.title)
         self.clock = pygame.time.Clock()
         self.ball = pygame.sprite.GroupSingle(Ball())
-        self.autoD = pygame.sprite.GroupSingle(AutoDeploy360(self.ball, 500, 100, 160, 10)) 
+        self.redeploy = pygame.sprite.GroupSingle(ReDeploy360(self.ball, 500, 100, 160, 10, 1000)) 
         self.running = False
 
     def run(self) -> None:
         self.running = True
-        self.autoD.sprite.deploy()
         while self.running:
             self.clock.tick(Settings.fps)
             self.watch_for_events()
@@ -87,6 +114,7 @@ class Game(object):
                     self.running = False
 
     def update(self) -> None:
+        self.redeploy.sprite.redeploy()
         self.ball.update()
     
     def draw(self) -> None:
